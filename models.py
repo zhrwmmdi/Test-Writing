5827ea61912cc0176b841b66bcf3f0e6441ab9ef
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-07-17 19:18:59 +0330

    documentation added to modules

--- models.py
+++ models.py
@@ -1,4 +1,16 @@
 class Book:
+    """
+    Represents a book in the library.
+
+    Attributes:
+        title (str): The title of the book.
+        author (str): The author of the book.
+        copies (int): The number of copies available in the library.
+
+    Methods:
+        __eq__(other): Checks equality with another Book instance.
+        __str__(): Returns a string representation of the book.
+    """
     def __init__(self, title, author, copies):
         self.title = title
         self.author = author
@@ -14,6 +26,19 @@ class Book:
 
 
 class User:
+    """
+    Represents a user of the library.
+
+    Attributes:
+        name (str): The name of the user.
+        borrowed_books (list): A list of books currently borrowed by the user.
+
+    Methods:
+        __eq__(other): Checks equality with another User instance.
+        borrow_book(book): Allows the user to borrow a book if available.
+        return_book(book): Allows the user to return a borrowed book.
+        __str__(): Returns a string representation of the user.
+    """
     def __init__(self, name):
         self.name = name
         self.borrowed_books = []
@@ -42,6 +67,19 @@ class User:
 
 
 class Library:
+    """
+    Represents the library management system.
+
+    Attributes:
+        books (list): A list of books available in the library.
+        users (list): A list of registered users in the library.
+
+    Methods:
+        add_book(book): Adds a book or a list of books to the library.
+        add_user(user): Adds a user or a list of users to the library.
+        find_book(title): Finds a book by title in the library.
+        find_user(name): Finds a user by name in the library.
+    """
     books = []
     users = []
 
